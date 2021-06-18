2b11343054803ae5daa822fc2bae045aa7a3efcb
Henrik Alsér <henrik.alser@me.com>
2021-06-18 23:46:02 +0200

    Cleanup

--- src/bin/srf04.rs
+++ src/bin/srf04.rs
@@ -91,7 +91,7 @@ mod app {
                     .checked_duration_since(&instant)
                     .and_then(|dur| dur.try_into().ok());
                 if let Some(Microseconds(t)) = diff {
-                    defmt::info!("Distance: {} cm", t / 58);
+                    defmt::info!("Distance: {} cm", t as f32 / 58.0);
                 }
             }
         }
