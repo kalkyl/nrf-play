6a2371c6a0c86ac82562c83df4bdd240f6844ce4
Henrik Alsér <henrik.alser@me.com>
2021-06-18 15:52:29 +0200

    Cleanup

--- src/bin/srf04.rs
+++ src/bin/srf04.rs
@@ -89,7 +89,7 @@ mod app {
                     .checked_duration_since(&instant)
                     .and_then(|dur| dur.try_into().ok());
                 if let Some(Microseconds(t)) = diff {
-                    defmt::info!("Distance: {} cm", t / 29_412);
+                    defmt::info!("Pulse length: {} us", t);
                 }
             }
         }
